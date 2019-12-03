08f8fc05a7db66a338f3c5cfa086aa9347159716
José Maria Andrade <2170819@my.ipleiria.pt>
2019-12-03 15:19:45 +0000

    Component movementList

--- resources/js/components/movement/movementList.vue
+++ resources/js/components/movement/movementList.vue
@@ -1,5 +1,26 @@
 <template>
-
+    <table class="table table-striped">
+        <thead>
+        <tr>
+            <th>Type</th>
+            <th>Category</th>
+            <th>Date</th>
+            <th>Value</th>
+            <th>Start Balance</th>
+            <th>End Balance</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="movement in movements" :key="movement.id">
+            <td>{{movement.type}}</td>
+            <td>{{movement.category}}</td>
+            <td>{{movement.date}}</td>
+            <td>{{movement.value}}</td>
+            <td>{{movement.start_balance}}</td>
+            <td>{{movement.end_balance}}</td>
+        </tr>
+        </tbody>
+    </table>
 </template>
 
 <script>
